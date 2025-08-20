c6acd0b5f9d0f13e95161cf5e98a656b9e5ee9b8
Musholic <musholic.tbg@gmail.com>
2025-08-20 19:00:47 +0200

    Fix import of unique with duplicate names + Update the letools import mapping of bases and affixes

--- gameFileExtractScript/extractLEToolsImportData.js
+++ gameFileExtractScript/extractLEToolsImportData.js
@@ -1,7 +1,19 @@
 // https://www.lastepochtools.com/profile/js/script.js
 function le_e(a) {
-    return null != a.displayNameKey ? (a = le_c(a.displayNameKey),
-    le_wa(a)) : a.displayName
+  return null != a.displayNameKey
+    ? ((a = le_c(a.displayNameKey)), le_wa(a))
+    : a.displayName;
 }
-JSON.stringify(Object.fromEntries(Object.entries(le_k).map(([k, v]) => [k, le_e(v)])))
-JSON.stringify(Object.fromEntries(Object.entries(le_n).concat(Object.entries(le_m)).map(([k, v]) => [k, v.affixId])))
+JSON.stringify(Object.fromEntries(Object.entries(le_k).map(([k, v]) => [k, {
+  "baseTypeId": v.baseTypeId,
+  "subTypeId": v.subTypeId,
+  "uniqueId": v.uniqueId
+}])));
+
+JSON.stringify(
+  Object.fromEntries(
+    Object.entries(le_n)
+      .concat(Object.entries(le_m))
+      .map(([k, v]) => [v.id, v.affixId]),
+  ),
+);
