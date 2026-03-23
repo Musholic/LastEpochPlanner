5a51a25bacdc482c61baef7f6913ac88467557ef
Musholic <musholic.tbg@gmail.com>
2026-03-23 17:18:30 +0100

    Add a test for the last idol offline import

--- spec/System/TestItemImport_spec.lua
+++ spec/System/TestItemImport_spec.lua
@@ -1,4 +1,4 @@
-describe("Item Import", function()
+describe("Offline Item Import", function()
     before_each(function()
         newBuild()
     end)
@@ -74,5 +74,51 @@ describe("Item Import", function()
 
         assert.are.equals("Grand Embers of Immortality", item.name)
         assert.are.equals("Blessing 10", item.slotName)
+    end)
+
+    it("should process last idol slot correctly", function()
+        local itemDataJson = [[
+        {
+          "itemData": null,
+          "data": [
+            5, 51, 219, 26, 1, 2, 9, 255, 255, 255, 0, 2, 3, 75, 255, 3, 86, 255, 0
+          ],
+          "inventoryPosition": { "x": 3, "y": 4 },
+          "quantity": 1,
+          "containerID": 29,
+          "formatVersion": 2
+        }
+        ]]
+        local item = build.importTab:processItemData(processJson(itemDataJson))
+
+        local expected = {
+            ["base"] = {
+                ["affixEffectModifier"] = -0.83,
+                ["baseTypeID"] = 26,
+                ["implicits"] = { },
+                ["req"] = {
+                  ["level"] = 0 },
+                ["subTypeID"] = 1,
+                ["type"] = 'Minor Idol' },
+              ["baseName"] = 'Minor Weaver Idol',
+              ["explicitMods"] = { },
+              ["implicitMods"] = { },
+              ["name"] = 'Minor Weaver Idol',
+              ["prefixes"] = {
+                [1] = {
+                    ["modId"] = '854_0',
+                    ["range"] = 255
+                }
+              },
+              ["rarity"] = 'RARE',
+              ["slotName"] = 'Idol 20',
+              ["suffixes"] = {
+                [1] = {
+                    ["modId"] = '843_0',
+                    ["range"] = 255
+                }
+              }
+        }
+        assert.are.same(expected, item)
     end)
 end)
